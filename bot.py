bbe686ee7a36996c8e28885872d38763ccfd323e
anuragmirrorleech <115845369+anuragmirrorleech@users.noreply.github.com>
2022-11-14 08:10:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,12 +6,12 @@ API_ID = int(environ.get("API_ID", "15939361"))
 API_HASH = environ.get("API_HASH", "f8beb0bd0054a717d84fbe9be12a23ea")
 BOT_TOKEN = environ.get("BOT_TOKEN", "5480565679:AAFHfDkrFbqB7WQ2VbYvt7orj5kPcNCejN8")
 SESSION = environ.get("SESSION", "BQAm9A0N3MQMs42zER-1amPJVszw_CPcPOH9zoV6NYZdblkb2NEWnrA6iRxXUu4qrRqwKxCHjJ1Nz3GRs5R7lEYyM8D6FZClw654BtQo3PMpBxXToSsmAz3kJ_5308dv6I045irdd63zr5arK5OUyvPJWomICXaLhiuontNEVBTFXK2lOsh-4Cz3ebXPFFqXROZIhGIosmqF7bVz0MoB0mf2Pt1_N55Yw1x7ADBf7x80Tl-al0l01U54DppXmf3BMbI719BbBZ-Mfycsu3xzmjVzflz3mN57iX_LXydxRAFAVZFE9iBrRwpjLOHMPac4k0ij3VAXqWsx9oTak4eD6yUrOg1h6AA")
-TIME = int(environ.get("TIME"))
+TIME = int(environ.get("TIME", "200"))
 GROUPS = []
-for grp in environ.get("GROUPS").split():
+for grp in environ.get("GROUPS", "-1001388131154 -1001517645234").split():
     GROUPS.append(int(grp))
 ADMINS = []
-for usr in environ.get("ADMINS").split():
+for usr in environ.get("ADMINS", "5543917190").split():
     ADMINS.append(int(usr))
 
 START_MSG = "<b>Hai {},\nI'm a private bot of @Am_Robots to delete group messages after a specific time</b>"
